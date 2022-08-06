d4c5dd573fab3e996f2249f56bf3c28430b9e397
Gabriel <flaviogabrielsr0507@gmail.com>
2022-08-06 16:46:31 -0300

    add context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Layout } from '../services/layout'
+import AppContext from '../services/context'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+
   return (
+    <AppContext.Provider value={{}} >
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </AppContext.Provider>
   )
 }
 
